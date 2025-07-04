54943ae3f7d1b7de6762af510907e0f85ac99b76
MichalBiesek <michal.biesek@itsilesia.com>
2025-07-04 20:38:08 +0200

    first commit
    first commit

---
+++ Assets/Room.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+[Serializable]
+public enum SideType
+{
+    Wall,
+    CDoor,
+    ODoor,
+    Nothing
+}
+
+
+[Serializable]
+public struct Side
+{
+    public SideType type;
+    public Room other_room;
+    public edge wall;
+};
+
+
+public class Room : MonoBehaviour
+{
+    public List<Side> near = new();
+    public List<Vector2Int> dir=new();
+    public bool is_rot=false;
+    public KeyCode trigger = KeyCode.Q;
+
+    bool pre = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (is_rot)
+        {
+            if ( Input.GetKeyDown(trigger) && pre == false ) {
+                pre=true;
+                Debug.Log("ROTATE!!!!!!!!!!");
+                rotate();
+            }
+            else if( Input.GetKeyDown(trigger) == false && pre )
+            {
+                pre = false;
+            }
+        }
+            
+    }
+    void rotate()
+    {
+        List<Side> nearO = new();
+        nearO.Add(near[0]);
+        nearO.Add(near[1]);
+        nearO.Add(near[2]);
+        nearO.Add(near[3]);
+
+        for(int idO=0;idO<4;idO++){
+            int id = (idO + 1) % 4;
+            Side s;
+            s.other_room = nearO[id].other_room;
+            s.wall = nearO[idO].wall;
+            s.type = nearO[idO].type;
+            near[id] = s;
+            int nid = id ^ 2;
+            if (s.other_room != null)
+            {
+                Side sn = s.other_room.near[nid];
+                sn.wall= s.wall;
+                sn.type = s.type;
+                s.other_room.near[nid] = sn;
+            }
+
+            if(nearO[idO].wall!= null)
+            {
+                edge e = nearO[idO].wall;
+                e.transform.RotateAround(new Vector3(0,1,0),-(float)Math.PI/2.0f);
+                Vector2Int d = dir[id];
+                e.transform.position = this.transform.position+new Vector3(d.x,0,d.y);
+            }
+        }
+        
+
+
+    }
+}
